cad6f1f4666d3e2dce52ab556bb0a9ed8897a640
farrel121 <31021522+farrel121@users.noreply.github.com>
2019-02-19 02:21:09 -0300

    Add files via upload

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -5,6 +5,7 @@ class CalcController {
     //initialize em portugues = iniciaçao
 
     constructor(){
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -22,17 +23,181 @@ class CalcController {
         }, 1000);
     }
 
+    //a função abaixo serve para adicionar varios eventos no botao
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event =>{
+
+            element.addEventListener(event, fn, false);
+
+        })
+
+
+
+    }
+    //método para pegar o ultimo operador
+    getLastOperation(){
+        return this._operation[this._operation.length-1];
+    }
+    //meotdo para saber se o ultimo digitado é um operador
+    //indexOf serve para procurar algo no array, se encontrar ele manda o index
+    isOperator(value){
+        return (['+','-','*','%','/'].indexOf(value) > -1);
+    }
+
+    //método para quando clicar no botao AC, limpa tudo
+    clearAll(){
+        this._operation = [];
+        this.displayCalc = "";
+    }
+    //método para quando clicar no botao CE, limpa o ultimo valor digitado
+    clearEntry(){
+        //o pop deleta um valor na ultima posição de um array
+        this._operation.pop();
+        this.displayCalc = "";
+    }
+
+    //metodo para apresentar mensagem de erro, no caso o deafault do switch
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length-1] = value;
+    }
+    
+    //metodo push para adicionar o operador na ultima posição do vetor
+    pushOperation(value){
+        this._operation.push(value); 
+        
+        if(this._operation.length > 3){
+            this.calc();
+        }
+    }
+
+    //vai atualizando os numeros assim que forem digitados no display
+    setLastNumberToDisplay(){
+        let lastNumber;
+
+        for (let i = this._operation.length-1; i >= 0; i--){
+
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+        this.displayCalc = lastNumber;
+    }
+    
+    addOperation(value){
+        if(isNaN(this.getLastOperation())){
+            if(this.isOperator(value)){
+                //se o anterior for um operador, troca o operador
+                this.setLastOperation(value);                         
+            }else if(isNaN(value)){
+                console.log("outra coisa" + value);
+            }else{               
+                this.pushOperation(value);
+                this.setLastNumberToDisplay(); 
+            }
+        }else{
+            if(this.isOperator(value)){
+                //se for um operador adiciona final do vetor
+                this.pushOperation(value);      
+            }else{
+                let newValue = this.getLastOperation().toString() + value.toString();               
+                this.setLastOperation(parseInt(newValue));
+                this.setLastNumberToDisplay();
+            }
+        }
+        console.log(this._operation);
+    }
+
+    //metodo que calcula
+    calc(){
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+        this._operation = [result, last];
+        this.setLastNumberToDisplay();
+    }
+
+    execBtn(value){
+        
+        switch (value){
+            case "ac":
+                this.clearAll();
+                break;
+            case "ce":
+                this.clearEntry();
+                break;
+            case "soma":
+                this.addOperation("+");
+                break;
+        
+            case "porcento":
+                this.addOperation("%");
+                break;
+
+            case "subtracao":
+                this.addOperation("-");
+                break;
+
+            case "divisao":
+                this.addOperation("/");
+                break;
+
+            case "multiplicacao":
+                this.addOperation("*");
+                break;
+
+            case "igual":
+                this.addOperation("=");
+                break;
+            case "ponto":
+                this.addOperation(".");
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            
+            default:
+                this.setError();
+        }
+
+    }
+    //inicia os eventos dos botoes, no caso click e passar o mause em cima vira a maozinha
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) =>{
-
-            btn.addEventListener('click', e =>{
+            //click drag é quando vc clica e arrasta
+            this.addEventListenerAll(btn, 'click drag', e =>{
                 //replace serve para substituir, no caso substitui o btn- por nada ""
-                console.log(btn.className.baseVal.replace("btn-", ""));
-            })
+                let txtBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(txtBtn);
+
+            });
+
+            //troca o cursor para a mãozinha
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+                btn.style.cursor = "pointer";
+
+            });            
         })
     }
+
+    
 
     setDisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale , {
